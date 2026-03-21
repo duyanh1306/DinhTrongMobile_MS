a12dd72d940542c5e0690eeb5fc6af9337c389e8
Kien <kienga445@gmail.com>
2026-03-21 22:08:38 +0700

    50% transfer request, scanable with webcam

--- client/src/App.js
+++ client/src/App.js
@@ -49,6 +49,7 @@ import AdminRecipe from "./pages/admin/AdminRecipe";
 
 import ManagerDashboard from "./pages/manager/ManagerDashboard";
 import ImportInventory from "./pages/manager/ImportInventory";
+import ManagerTransferRequest from "./pages/manager/ManagerTransferRequest";
 const CustomerProfile = () => (
     <h2 className="text-xl font-bold">Thông tin tài khoản khách hàng</h2>
 );
@@ -401,6 +402,16 @@ function App() {
             <PrivateRoute allowedRoles={["MANAGER"]}>
               <ManagerLayout>
                 <ImportInventory/>
+              </ManagerLayout>
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/manager/transfer_requests"
+          element={
+            <PrivateRoute allowedRoles={["MANAGER"]}>
+              <ManagerLayout>
+                <ManagerTransferRequest/>
               </ManagerLayout>
             </PrivateRoute>
           }
