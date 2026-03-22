75dead5196c7f9a0ce50f27b81580577d389dd22
Kien <kienga445@gmail.com>
2026-03-22 18:11:43 +0700

    75% transfer request?, fix logic, validate

--- server/routes/transfer_requestRoutes.js
+++ server/routes/transfer_requestRoutes.js
@@ -1,10 +1,25 @@
 
 const express = require("express");
 const router = express.Router();
-const { getAllTransferRequests, getTransferRequestDetailsById, createTransferRequest } = require("../controllers/transfer_requestController");
+const {
+    getAllTransferRequests,
+    getTransferRequestDetailsById,
+    createTransferRequest,
+    getTransferRequestById,
+    confirmShipment,
+    confirmReceipt,
+    approveTransferRequest,
+    rejectTransferRequest
+} = require("../controllers/transfer_requestController");
 
 router.get("/", getAllTransferRequests);
 router.get("/:id/details", getTransferRequestDetailsById);
 router.post("/", createTransferRequest);
+router.get("/:id", getTransferRequestById);
+router.put("/:id/confirm-shipment", confirmShipment);
+router.put("/:id/confirm-receipt", confirmReceipt);
+router.put("/:id/approve", approveTransferRequest);
+router.put("/:id/reject", rejectTransferRequest);
+
 
 module.exports = router;
