b0aa16da68f697a8b2488281d7f6e737fd1922f8
thanhtmhe186559 <thanhtmhe186559@fpt.edu.vn>
2026-04-05 23:23:34 +0700

    hot fix 3

--- server/seedLocation.js
+++ server/seedLocation.js
@@ -2,29 +2,16 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 const axios = require('axios'); 
 const VietnamLocation = require('./models/VietnamLocation'); 
-
+const PROVINCES_API_URL = process.env.PROVINCES_API_URL
 const MONGO_URI = process.env.MONGO_URI; 
 
 const seedData = async () => {
     try {
-        if (!MONGO_URI) {
-            console.error("❌ Lỗi: Không tìm thấy MONGO_URI trong file .env");
-            process.exit(1);
-        }
-
-        console.log("Đang kết nối tới MongoDB Atlas...");
         await mongoose.connect(MONGO_URI);
-        console.log("Kết nối thành công!");
         await VietnamLocation.deleteMany({});
-        
-        console.log("Đang tải dữ liệu từ Provinces Open API...");
-        const response = await axios.get('https://provinces.open-api.vn/api/?depth=3');
+        const response = await axios.get(PROVINCES_API_URL);
         const locations = response.data;
-        
-        console.log(`Đã tải xong ${locations.length} Tỉnh/Thành phố. Đang lưu vào Database...`);
         await VietnamLocation.insertMany(locations);
-
-        console.log("Đã nạp toàn bộ dữ liệu địa lý Việt Nam vào Database thành công!");
         process.exit(0);
     } catch (error) {
         console.error("Có lỗi xảy ra:", error.message);
